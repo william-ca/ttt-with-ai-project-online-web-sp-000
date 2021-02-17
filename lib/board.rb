9bab287ddbd5e8d5e914c79f350b840998b6c89b
Will Andre <wandre94@gmail.com>
2021-02-17 02:24:41 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,50 @@
+class Board
+
+  attr_accessor :cells
+
+  @cells = []
+
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+
+  def initialize
+    reset!
+  end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(input)
+    self.cells[input.to_i-1]
+  end
+
+  def full?
+    self.cells.all? { |cell| cell == "X" || cell == "O" }
+  end
+
+  def turn_count
+    self.cells.count("X") + self.cells.count("O")
+  end
+
+  def taken?(position)
+    self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position)
+    position.to_i <= 9 && position.to_i >= 1 &&
+    self.cells[position.to_i-1] == " "
+  end
+
+  def update(position, player)
+    self.cells[position.to_i-1] = player.token
+  end
+
+
+
+end
